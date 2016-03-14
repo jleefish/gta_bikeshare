a7d0be834c5176b6f2e4a0cb0323c01b1f00bb9a
jleefish <jhlee@jhs-MacBook-Pro.local>
2016-03-14 15:37:29 -0400

    update js for map

--- assets/js/views/map.js
+++ assets/js/views/map.js
@@ -24,7 +24,8 @@
     };
 
     map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions); 
-
+    google.maps.event.addDomListener(window, 'load', initialize);
+    
     //adding markers    
     for (var i = 0; i < stationList.length; i++) {
       var position = new google.maps.LatLng(stationList[i].latitude, stationList[i].longitude);
